f25a624778d27fb799836ae5ed4874368f942c4a
Bárbara Bitarello <barbara.bitarello@pennmedicine.upenn.edu>
2020-07-22 16:27:50 -0400

    Update readIDs.js

--- scripts/readIDs.js
+++ scripts/readIDs.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 try {
-  let data = fs.readFileSync('rsIDs.txt', 'utf8');
+  let data = fs.readFileSync('input/rsIDs_whi.txt', 'utf8');
   let arr = data.split('\n');
-  fs.writeFile('rsIDs.json', JSON.stringify(arr), (err) => {
+  fs.writeFile('input/rsIDs_whi.json', JSON.stringify(arr), (err) => {
     if (err) console.log(err);
   })
 } catch(e) {
