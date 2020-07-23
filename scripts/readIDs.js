49a770009361d052e6ba1da0f801d067d5353eae
Bárbara Bitarello <barbara.bitarello@pennmedicine.upenn.edu>
2020-07-23 14:54:55 -0400

    Update readIDs.js

--- scripts/readIDs.js
+++ scripts/readIDs.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
 
+if (process.argv.length === 2) {
+  console.error('I expected one argument! It should be the name of a file with SNP IDs, one per line.');
+  process.exit(1);
+} 
+
+const inputIndex = process.argv.indexOf('--input');
+// Checks for --output and if we have a value
+if (inputIndex > -1) {
+  // Grabs the value after --input
+  inputValue = process.argv[inputIndex + 1];
+}
+
+var path = process.cwd();
+
+let my_input= path + "/" + inputValue;
 try {
-  let data = fs.readFileSync('input/rsIDs_jhs.txt', 'utf8');
+  let data = fs.readFileSync(my_input, 'utf8');
   let arr = data.split('\n');
   fs.writeFile('input/rsIDs_jhs.json', JSON.stringify(arr), (err) => {
     if (err) console.log(err);
